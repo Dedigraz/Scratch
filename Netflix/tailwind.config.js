677d824c85f902556c0685a934b2a31aad91116f
Ebunrere Adebanjo <ebunrereadebanjo@outlook.com>
2024-02-12 08:00:50 +0100

    reactified the netflix app for variety reasons

--- Netflix/tailwind.config.js
+++ Netflix/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-};
+  plugins: [],
+}
